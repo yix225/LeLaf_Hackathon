c8e515a77c004dc44b1679d20fc33463fe95aa21
PapaAsibuo <pka224@lehigh.edu>
2023-11-11 22:57:28 -0500

    GET Requests work to pull messages in now

--- LeLaf_Hackathon/CommunityScreen.js
+++ LeLaf_Hackathon/CommunityScreen.js
@@ -112,7 +112,13 @@ const CommunityScreen = ({ navigation }) => {
         <TouchableOpacity
         style={styles.item}
         onPress={() => 
-          console.log(item.path)
+          axios.get('http://172.20.10.7:3000/allPosts/' + item.path)
+          .then(response => {
+            console.log('Response:', response.data);
+          })
+          .catch(error => {
+            console.log(error.response.data);
+          })
           
 
 
